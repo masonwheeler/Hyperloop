9c1d31a1f9de6bb67bf8ef96fb3e2f3a4c8c52ef
Jon <jward@college.harvard.edu>
2015-04-22 17:48:03 -0400

    fixed the projection module

--- genpaths/proj.py
+++ genpaths/proj.py
@@ -1,36 +1,54 @@
 """
-Jonathan Ward 3/24/2015
-
-This file contains the function definitions for generating and validating
-routes on the lattice.
+Admin: Jonathan Ward 
+Last Mod: 4/22/2015
+Description: This file contains the function definitions for creating and using map projections
+Most Recent Change: swapped latitude and longitude into correct positions
 
 TODO:
 Add arguments to get_omerc    
 Add conical Albers proj    
 
-Most Recent Change: added lat_to_xy and xy_to_latlon
+
 """
 
 import mpl_toolkits.basemap.pyproj as pyproj
 
-def omerc_proj(startLatLon, endLatLon):
-    startLat, startLon = startLatLon
-    endLat, endLon = endLatLon
-    centerLat = (startLat + endLat)/2
-    centerLon = (startLon + endLon)/2
-    omerc=pyproj.Proj('+proj=omerc +lon_0=' + centerLon + ' +lat_0=' + centerLat  + ' +lon_2=' + endLon + ' +lat_2= ' + endLat + ' +lon_1=' + startLon + ' +lat_1=' + startLat)
+def omerc_proj(startLonLat, endLonLat):
+    startLon, startLat = startLonLat
+    startLatStr = str(startLat)
+    startLonStr = str(startLon)
+    endLon, endLat = endLonLat
+    endLatStr = str(endLat)
+    endLonStr = str(endLon)
+    centerLat = (startLat + endLat)/2.0
+    centerLatStr = str(centerLat)
+    centerLon = (startLon + endLon)/2.0
+    centerLonStr = str(centerLon)
+    omerc=pyproj.Proj('+proj=omerc +lon_0=' + centerLonStr + ' +lat_0=' + centerLatStr  + ' +lon_2=' + endLonStr + ' +lat_2= ' + endLatStr + ' +lon_1=' + startLonStr + ' +lat_1=' + startLatStr)
     return omerc
 
 def mrlc_proj():
     mrlc=pyproj.Proj("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs")
     return mrlc
 
-def latlon_to_xy(latlon,proj):
-    lat, lon = latlon
-    xy = proj(lat, lon)
+def lonlat_to_xy(lonlat,proj):
+    lon, lat = lonlat
+    xy = proj(lon, lat)
     return xy
 
-def xy_to_latlon(xy,proj):
+def xy_to_lonlat(xy,proj):
     x, y = xy
-    latlon = proj(x, y, inverse=True)
-    return latlon
+    lonlat = proj(x, y, inverse=True)
+    return lonlat
+
+startLonLat = [-118.25, 34.05]
+endLonLat = [-122.4167, 37.7833]
+centerLonLat = [(startLonLat[0] + endLonLat[0])/2.0, (startLonLat[1] + endLonLat[1])/2.0]
+omerc = omerc_proj(startLonLat,endLonLat)
+mrlc = mrlc_proj()
+startXY = lonlat_to_xy(startLonLat,omerc)
+#print(startXY)
+endXY = lonlat_to_xy(endLonLat,omerc)
+#print(endXY)
+centerXY = lonlat_to_xy(centerLonLat,omerc)
+#print(centerXY)
