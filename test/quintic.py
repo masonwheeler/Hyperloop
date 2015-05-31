bd2ca1b6158d66b405495a76c5655802ef039729
David Benjamin Roberts <Droberts@Davids-MacBook-Pro.local>
2015-05-31 19:17:57 -0400

    interpolation.py done, which provides automatic piecewise, timewise, C^3 quintic polynomial interpolation. locally minimizes total jerk as demonstrated by Euler-Lagrange equations.

---
+++ test/quintic.py
@@ -0,0 +1,58 @@
+import math
+import numpy as np
+import time
+import scipy
+import scipy.interpolate
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import radiusOfCurvature as rad
+
+def Gamma(t):
+	return np.array([[1, t, t**2, t**3, t**4, t**5], 
+		[0, 1, 2*t, 3*t**2, 4*t**3, 5*t**4],
+		[0, 0, 2, 6*t, 12*t**2, 20*t**3]])
+
+def GammaEn(t):
+	return np.array([[1, t, t**2, t**3, t**4, t**5], 
+		[0, 1, 2*t, 3*t**2, 4*t**3, 5*t**4],
+		[0, 0, 2, 6*t, 12*t**2, 20*t**3],
+		[0, 0, 0, 6, 24*t, 60*t**2],
+		[0, 0, 0, 0, 24, 120*t],
+		[0, 0, 0, 0, 0, 0]])
+
+def GammaSt(t):
+	return np.array([[0, 0, 0, 0, 0, 0], 
+		[0, -1, -2*t, -3*t**2, -4*t**3, -5*t**4],
+		[0, 0, -2, -6*t, -12*t**2, -20*t**3],
+		[0, 0, 0, -6, -24*t, -60*t**2],
+		[0, 0, 0, 0, -24, -120*t],
+		[1, t, t**2, t**3, t**4, t**5]])
+
+
+def interp(G):
+	t, x, dx0, d2x0, dxN, d2xN = G
+	N = len(x)-1
+	b0 = np.array([x[0], dx0, d2x0])
+	bN = np.array([x[N], dxN, d2xN])
+	def bj(j):
+		return np.array([x[j], 0, 0, 0, 0, x[j]])
+	C = np.zeros((6*N, 6*N))
+	b = np.zeros(6*N)
+	b[0:3] = b0
+	b[6*N - 3 :6*N] = bN
+	for j in range(1, N):
+		b[3 + 6*(j-1):3 + 6*j] = bj(j)
+	C[0:3,0:6] = Gamma(t[0])
+	C[6*N - 3:6*N, 6*N - 6:6*N] = Gamma(t[N])
+	for j in range(1, N):
+		C[3 + 6*(j-1) : 3 + 6*j,6*(j-1):6*j] = GammaEn(t[j])
+		C[3 + 6*(j-1) : 3 + 6*j,6*j:6*(j+1)] = GammaSt(t[j])
+	a = np.linalg.solve(C,b)
+	alist = np.zeros((N, 6))
+	for i in range(N):
+		alist[i] = a[6*i:6*(i+1)]
+	return alist.tolist()
+
+
+
+
---
+++ test/radiusOfCurvature.py
@@ -0,0 +1,41 @@
+import math
+import numpy as np
+import time
+import scipy
+import scipy.interpolate
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+R =  6.371*(10**6)
+
+def radius(three_points):
+	p1, p2, p3 = three_points
+	a = np.linalg.norm(np.subtract(p1, p2))
+	b = np.linalg.norm(np.subtract(p2, p3))
+	c = np.linalg.norm(np.subtract(p1, p3))
+	p = (a + b + c) / 2
+	A = math.sqrt(p * (p - a) * (p - b) * (p - c))
+	if A == 0:
+		return 1000000000000
+	else:
+		return a * b * c / (4 * A)
+
+def r_i(coordinates):
+    r_i_results = []
+    for coordinate in coordinates:
+        x_i = R*math.cos(coordinate[0])*math.cos(coordinate[1])
+        y_i = R*math.cos(coordinate[0])*math.sin(coordinate[1])
+        z_i = R*math.sin(coordinate[0])
+        r_i_results.append([x_i, y_i, z_i])
+    return r_i_results
+
+def t_i(v_i, r_i):
+    t_i_results = [0]
+    for i in range(len(v_i)):
+        t_i = t_i_results[i] + np.linalg.norm(np.subtract(r_i[i+1], r_i[i])) / v_i[i]
+        t_i_results.append(t_i)
+    return t_i_results    
+
+
+
+   
