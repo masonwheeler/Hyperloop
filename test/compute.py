20d47336aee058bead2e896b35704824382f183e
David Benjamin Roberts <Droberts@Davids-MacBook-Pro.local>
2015-06-14 16:07:19 -0400

    Fixing concatenation

--- test/compute.py
+++ test/compute.py
@@ -59,9 +59,9 @@ def interpolation_data(tht_i_phi_i):
    comfort = max(comfort_Ratings)
    triptime = t[-1]
    
-   plot_times = sum(*s)
-   joined_v = sum(*v)
-   joined_a = sum(*a)
+   plot_times = sum(s)
+   joined_v = sum(v)
+   joined_a = sum(a)
    pts = zip(intrp.Coeffs_to_Vals(ax, plot_times, t), intrp.Coeffs_to_Vals(ay, plot_times, t), intrp.Coeffs_to_Vals(az, plot_times, t))
    vpts = [np.linalg.norm(vel_vector) for vel_vector in v]
    apts = [np.linalg.norm(accel_vector) for accel_vector in a]
