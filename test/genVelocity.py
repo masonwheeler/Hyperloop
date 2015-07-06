3b4a0de26cc3c74b8cfb4df07220f92d614f8881
David Benjamin Roberts <Droberts@Davids-MacBook-Pro-2.local>
2015-07-06 13:22:30 -0400

    Two new modules: velocity generation module, and a spatial interpolation module. A reparametrization module is coming soon.

---
+++ test/genVelocity.py
@@ -0,0 +1,47 @@
+import math
+import numpy as np
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
+#import os
+
+#print os.getcwd()
+
+R =  6.371*(10**6)
+
+def mean(List):
+  return sum(List)/len(List)
+
+def pointstoRadius(three_points):
+  p1, p2, p3 = three_points
+  a = np.linalg.norm(np.subtract(p1, p2))
+  b = np.linalg.norm(np.subtract(p2, p3))
+  c = np.linalg.norm(np.subtract(p1, p3))
+  p = (a + b + c) / 1.99999999999999
+  A = math.sqrt(p * (p - a) * (p - b) * (p - c))
+  if A == 0:
+    return 1000000000000
+  else:
+    return a * b * c / (4 * A)
+
+def xPointstovPoints(xPoints):
+  sPoints = [0 for i in range(0,len(xPoints))]
+  for i in range(0, len(xPoints)-1):
+    sPoints[i+1] = sPoints[i] + np.linalg.norm(xPoints[i+1] - xPoints[i])
+  vPoints = [0,200,300] + [mean([min(math.sqrt(9.81*.3*pointstoRadius(xPoints[j:j+3])),330) for j in range(i-2,i+3)]) for i in range(3,len(xPoints)-4)] + [320, 300, 200, 0]
+  return [sPoints, vPoints]
+
+def vPointstovFunc(sPoints,vPoints):
+  return interp1d(sPoints, vPoints, kind='cubic')
+
+#xPoints = np.genfromtxt('/Users/Droberts/Dropbox/The Hyperloop/keys/route0.csv', delimiter=",")
+#sPoints, vPoints = xPointstovPoints(xPoints)
+#vFunc = vPointstovFunc(sPoints, vPoints)
+
+#sVals = np.arange(0, sPoints[-1], 10)
+#vVals = vFunc(sVals)
+#plt.plot(sPoints, vPoints, 'o', sVals, vVals, '-')
+#plt.show()
+
+
+
+
---
+++ test/spatInterp.py
@@ -0,0 +1,63 @@
+import math
+import numpy as np
+import quintic as quint
+import interpolation
+import genVelocity as gen
+#import matplotlib.pyplot as plt
+#import time
+
+R =  6.371*(10**6)
+
+def txPointstoxVals(tPoints, xPoints, n , mu):
+  xPoints = [point[mu] for point in xPoints]
+  N = n-1
+  M = int(math.ceil((len(tPoints)-1.)/ N))
+  G = [0 for i in range(M)]
+  if M == 1:
+  	G = [[tPoints, xPoints, 0, 0, 0, 0]]
+  elif M == 2:
+    G[0] = [tPoints[0:N+1],xPoints[0:N+1], 0, 0, (xPoints[N+1]-xPoints[N-1])/(tPoints[N+1]-tPoints[N-1]),0]
+    G[M-1] = [tPoints[0:N+1],xPoints[0:N+1], (xPoints[N+1]-xPoints[N-1])/(tPoints[N+1]-tPoints[N-1]),0,0,0]
+  else:
+    G[0] = [tPoints[0:N+1],xPoints[0:N+1],0,0, (xPoints[N+1]-xPoints[N-1])/(tPoints[N+1]-tPoints[N-1]),0]  
+    for k in range(2,M):
+      G[k-1] = [tPoints[(k-1)*N:k*N+1],xPoints[(k-1)*N:k*N+1], (xPoints[(k-1)*N+1]-xPoints[(k-1)*N-1])/(tPoints[(k-1)*N+1]-tPoints[(k-1)*N-1]),0,(xPoints[k*N+1]-xPoints[k*N-1])/(tPoints[k*N+1]-tPoints[k*N-1]),0]
+    G[M-1] = [tPoints[(M-1)*N:len(tPoints)], xPoints[(M-1)*N:len(xPoints)],(xPoints[(M-1)*N+1]-xPoints[(M-1)*N-1])/(tPoints[(M-1)*N+1]-tPoints[(M-1)*N-1]) ,0,0,0]
+  xCoeffs = sum([quint.interp(g) for g in G],[])
+  tVals = sum([[tPoints[j]+(i/128.)*(tPoints[j+1]-tPoints[j]) for i in range(2,128)] for j in range(len(tPoints)-1)],[])
+  xVals = interpolation.Coeffs_to_Vals(xCoeffs, tVals, tPoints)
+  return [tVals, xVals]
+
+def txPointstoxyVals(tPoints, xPoints, n):
+	return [txPointstoxVals(tPoints, xPoints, n, mu)[1] for mu in [0,1]]
+
+#start = time.time()
+#xPoints = np.genfromtxt('/Users/Droberts/Dropbox/The Hyperloop/keys/route2.csv', delimiter = ",")
+#sPoints, vPoints = gen.xPointstovPoints(xPoints)
+#tPoints = np.arange(10,200.000001,(200-10.)/(len(xPoints)-1))
+#xVals, yVals = txPointstoxyVals(tPoints, xPoints, 7)
+
+#plt.plot([p[0] for p in xPoints], [p[1] for p in xPoints], 'o', xVals, yVals, '-')
+#print time.time()-start
+#plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
