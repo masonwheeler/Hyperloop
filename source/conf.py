5312fa1fde0580b47ee7ed8f6587b7ac8006823b
Ubuntu <ubuntu@ip-172-31-10-108.ec2.internal>
2015-09-08 23:11:29 +0000

    updated docs

--- source/conf.py
+++ source/conf.py
@@ -31,7 +31,7 @@ sys.path.insert(0, os.path.abspath('../test'))
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
+    'sphinxcontrib.napoleon',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
@@ -118,7 +118,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
