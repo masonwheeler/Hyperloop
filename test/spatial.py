0736a24eabe3ac0e4b3706a17b6673faadba5dd2
jon ward <jward@college.harvard.edu>
2015-09-13 19:23:02 -0400

    visualization set up part a

--- test/spatial.py
+++ test/spatial.py
@@ -29,6 +29,7 @@ VISUALIZE_SPLINE = True
 VISUALIZE_LATTICE = True
 VISUALIZE_EDGES = True
 VISUALIZE_GRAPHS = True
+VISUALIZE_COST_TIME_SCATTERPLOT = True
 VISUALIZE_PATHS_2D = True
 
 def build_directions(start, end):
@@ -46,14 +47,14 @@ def build_spatial_lattice(route_directions):
     if config.VISUAL_MODE:
         if VISUALIZE_SPLINE:
             plottable_spline = route_spatial_lattice.get_plottable_spline()
-            config.PLOT_QUEUE.append([plottable_spline, 'r-'])
+            config.PLOT_QUEUE_SPATIAL_2D.append([plottable_spline, 'r-'])
         if VISUALIZE_DIRECTIONS:
             plottable_directions = \
                 route_spatial_lattice.get_plottable_directions()
-            config.PLOT_QUEUE.append([plottable_directions, 'b-'])
+            config.PLOT_QUEUE_SPATIAL_2D.append([plottable_directions, 'b-'])
         if VISUALIZE_LATTICE:
             plottable_lattice = route_spatial_lattice.get_plottable_lattice()
-            config.PLOT_QUEUE.append([plottable_lattice, 'g.'])
+            config.PLOT_QUEUE_SPATIAL_2D.append([plottable_lattice, 'g.'])
     return route_spatial_lattice
 
 def build_spatial_edges_sets(route_spatial_lattice):
@@ -65,7 +66,7 @@ def build_spatial_edges_sets(route_spatial_lattice):
     if config.VISUAL_MODE:
         if VISUALIZE_EDGES:
             plottable_edges = route_spatial_edges_sets.get_plottable_edges()
-            config.PLOT_QUEUE.append([plottable_edges, 'k-'])
+            config.PLOT_QUEUE_SPATIAL_2D.append([plottable_edges, 'k-'])
     return route_spatial_edges_sets
 
 def build_spatial_graphs_sets(route_spatial_edges_sets):
@@ -75,11 +76,16 @@ def build_spatial_graphs_sets(route_spatial_edges_sets):
                                                  route_spatial_edges_sets)
     if config.VISUAL_MODE:
         if VISUALIZE_GRAPHS:
-            plottable_graphs = route.spatial_graphs_sets.get_plottable_graphs()
+            plottable_graphs = route_spatial_graphs_sets.get_plottable_graphs()
             for plottable_graph in plottable_graphs:
                 config.PLOT_QUEUE.append([plottable_graph, 'c-'])
                 visualize.plot_objects(config.PLOT_QUEUE)
-                config.PLOT_QUEUE.pop()    
+                config.PLOT_QUEUE.pop()
+        if VISUALIZE_COST_TIME_SCATTERPLOT:
+            cost_time_scatter = route_spatial_graphs_sets.cost_time_scatter()
+            config.PLOT_QUEUE_COST_TIME_SCATTERPLOT.append(
+                                     [cost_time_scatter, 'r.'])
+            visualize.plot_objects(config.PLOT_QUEUE_COST_TIME_SCATTERPLOT)
     return route_spatial_graphs_sets
 
 def build_spatial_paths_set_2d(route_spatial_graphs_sets):
@@ -90,19 +96,30 @@ def build_spatial_paths_set_2d(route_spatial_graphs_sets):
     if config.VISUAL_MODE:
         if VISUALIZE_PATHS_2D:
             plottable_paths_2d = \
-                route.spatial_paths_set_2d.get_plottable_paths()
-            for plottable_path in plottable_paths_2d:
-                config.PLOT_QUEUE.append([plottable_path, 'c-'])
-                visualize.plot_objects(config.PLOT_QUEUE)
-                config.PLOT_QUEUE.pop()    
+                route_spatial_paths_set_2d.get_plottable_paths()
+            plottable_paths_graphs_2d = \
+                route_spatial_paths_set_2d.get_paths_graphs()
+            plottable_paths_and_graphs = zip(plottable_paths_2d,
+                                             plottable_paths_graphs_2d)
+            for plottable_path_and_graph in plottable_paths_and_graphs:
+                plottable_path, plottable_graph = plottable_path_and_graph
+                config.PLOT_QUEUE_SPATIAL_2D.append([plottable_path, 'c-'])
+                config.PLOT_QUEUE_SPATIAL_2D.append([plottable_graph, 'y-'])
+                visualize.plot_objects(config.PLOT_QUEUE_SPATIAL_2D)
+                config.PLOT_QUEUE_SPATIAL_2D.pop()
+                config.PLOT_QUEUE_SPATIAL_2D.pop()
     return route_spatial_paths_set_2d
 
-#def build_spatial_paths_3d(route_spatial_paths_set_2d):
-#    """Build the best tube elevation options for a given 2d spatial path
-#    """
-#    route_spatial_paths_set_3d = spatial_paths_2d.get_spatial_paths_2d(
-#                                            route_spatial_paths_set_2d)
-#    return route_spatial_paths_set_3d
+def build_spatial_paths_3d(route_spatial_paths_set_2d):
+    """Build the best tube elevation options for a given 2d spatial path
+    """
+    route_spatial_paths_set_3d = spatial_paths_2d.get_spatial_paths_2d(
+                                            route_spatial_paths_set_2d)
+    if config.VISUAL_MODE:
+        if VISUALIZE_PATHS_3D:
+            plottable_paths_3d = \
+                route_spatial_paths_set_3d.get_plottable_paths()            
+    return route_spatial_paths_set_3d
 
 def city_pair_to_spatial_graphs_sets(start, end):
     route_directions = build_directions(start, end)
