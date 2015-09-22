50506324bc1095013ee67a9d7401e7e7e97a510e
jon ward <jward@college.harvard.edu>
2015-09-21 20:17:49 -0400

    removed unnecessary files

--- test/saver.py
+++ test/saver.py
@@ -4,30 +4,6 @@ Original Developer: Jonathan Ward
 
 DROPBOX_DIRECTORY = "/home/ubuntu/Dropbox/save"
 
-"""
-def save_routes(routes, start, end, start_lat_lng, end_lat_lng):
-    routes_dicts = []
-    route_index = 0
-    for route in routes:
-        route_dict = route.as_dict(route_index)
-        route_index += 1
-        routes_dicts.append(route_dict)
-    city_pair = {
-        "startCity": {
-            "name": start,
-            "coordinates": start_lat_lng
-        },
-        "endCity": {
-            "name": end,
-            "coordinates": end_lat_lng
-        },
-        "routes": routes_dicts
-    }
-    filename = "/" + str(start) + "_" + str(end) + ".json"
-    save_path = DROPBOX_DIRECTORY + filename
-    with open(save_path, 'w') as file_path:
-        json.dump(city_pair, file_path)
-"""
 
 def save_routes_set(routes_set):
     """Saves completed routes to json
