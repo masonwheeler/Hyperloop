4a53f377fdc621c5d05975d352f913665fa5044e
jon ward <jward@college.harvard.edu>
2015-09-24 13:33:33 -0400

    refactoring

--- test/tube_edges.py
+++ test/tube_edges.py
@@ -10,6 +10,7 @@ import parameters
 import pylon_cost
 import tube_cost
 
+
 def compute_tube_degree_constraint(pylon_arc_length_spacing,
                                      pylon_height_step_size,
                                           tube_interpolator):
@@ -27,23 +28,13 @@ class TubeEdge(abstract_edges.AbstractEdge):
         abstract.AbstractEdge.__init__(self, start_pylon, end_pylon)
         self.tube_coords = [start_pylon.tube_coords, end_pylon.tube_coords]
         self.tube_cost = tube_cost.compute_pylon_cost(self.tube_coords)
-        self.pylon_cost = self.pylon_cost(start_pylon, end_pylon)
-
+        self.pylons_costs = [pylon_cost.compute_pylon_cost(start_pylon),
+                             pylon_cost.compute_pylon_cost(end_pylon)]
 
 class TubeEdgesSets(abstract.AbstractEdgesSets):
 
-    def tube_edge_builder(self, start_pylon, end_pylon):
-        tube_edge = TubeEdge(start_pylon, end_pylon)
-        return tube_edge
-
-    @staticmethod
-    def is_tube_edge_pair_compatible(tube_edge_a, tube_edge_b):
-        edge_pair_compatible = abstract.AbstractEdgesSets.is_edge_pair_compatible(
-            tube_edge_a, tube_edge_b, config.TUBE_DEGREE_CONSTRAINT)
-        return edge_pair_compatible
-
-    def __init__(self, pylons_lattice):
+    def __init__(self, pylons_lattice, tube_degree_constraint):
         abstract.AbstractEdgesSets.__init__(self, pylons_lattice,
-                                            self.tube_edge_builder,
-                                            self.is_tube_edge_pair_compatible)
+                                            TubeEdge,
+                                            tube_degree_constraint)
 
