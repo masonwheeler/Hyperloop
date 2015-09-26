ce7bed50fb564ed99b7c5be0cdbb67db50d8dcab
Ubuntu <ubuntu@ip-172-31-10-108.ec2.internal>
2015-09-26 21:31:19 +0000

    velocity refactoring part c

--- test/velocity.py
+++ test/velocity.py
@@ -50,47 +50,9 @@ def velocities_by_arc_length_to_velocities_by_time(velocities_by_arc_length,
     excess_velocities = np.multiply(excess_times, selected_slopes)
     velocities_by_time = np.add(excess_velocities, 
                                 selected_velocities_by_arc_length)
-    return velocities_by_time
+    return [velocities_by_time, cumulative_time_steps]
 
-def compute_local_trip_time_excess(max_allowed_velocities,
-                                   velocity_arc_length_step_size):
-    num_velocities = max_allowed_velocities.size
-    max_possible_velocities = np.empty(num_velocities)
-    max_possible_velocities.fill(config.MAX_SPEED)
-    minimum_possible_trip_time = compute_trip_time(max_possible_velocities,
-                                                   velocity_arc_length_step_size)
-    minimum_allowed_trip_time = compute_trip_time(max_allowed_velocities,
-                                                  velocity_arc_length_step_size)
-    local_trip_time_excess = minimum_allowed_trip_time - minimum_possible_trip_time
-    return local_trip_time_excess
-
-def compute_max_endpoint_velocities(max_linear_accel, max_possible_velocity,
-                                    velocity_arclength_step_size):
-    velocity = 0
-    max_end_point_velocities = []
-    while velocity < max_possible_velocity:
-        max_end_point_velocites.append(velocity)
-        velocity = np.sqrt(2 * velocity_arc_length_step_size * max_linear_accel
-                           + np.square(current_velocity))
-    return max_end_point_velocities
-
-def global_max_allowed_velocities(local_max_allowed_velocities,
-                                  max_end_point_velocities):
-    endpoint_velocities_length = max_end_point_velocities.length
-    max_start_velocities = max_end_point_velocities
-    max_end_velocities = max_end_point_velocities[::-1]
-    local_max_start_velocities = local_max_allowed_velocities[
-        :endpoint_velocities_length]
-    local_max_end_velocities = local_max_allowed_velocities[
-        -endpoint_velocities_length:]
-    effective_max_start_velocities = np.minimum(max_start_velocities,
-                                                local_max_start_velocities)
-    effective_max_end_velocities = np.minimum(max_end_velocities,
-                                              local_max_end_velocities)
-    global_max_allowed_velocities = local_max_allowed_velocities
-    global_max_allowed_velocities[:endpoint_velocities_length] = \
-        effective_max_start_velocities
-    global_max_allowed_velocities[-endpoint_velocities_length:] = \
-        effective_max_end_velocities
-    return global_max_allowed_velocities
+def constrain_velocity_profile_longitudinal_accelerations(velocities_by_time,
+                  time_step_size, max_longitudinal_acceleration):
+    pass
 
