7689a911eee78addcd4e1acb72c72720452ddd57
jon ward <jward@college.harvard.edu>
2015-10-28 17:44:32 -0400

    cache refactoring

--- test/cacher.py
+++ test/cacher.py
@@ -43,7 +43,6 @@ def create_basefolders():
     if not os.path.exists(save_directory):
         os.makedirs(save_directory)
 
-
 def create_workingcachename(start, end):
     """Names the cache folder for a given city pair"""
     working_cache_name = "_".join([start, "to", end])
@@ -51,14 +50,13 @@ def create_workingcachename(start, end):
     return working_cache_name
 
 
-def create_workingsavedirname(start, end):
-    """Names the save folder for a given city pair"""
-    working_save_dir_name = "_".join([start, "to", end])
-    config.WORKING_SAVE_DIR_NAME = working_save_dir_name
-    return working_save_dir_name
+#def create_workingsavedirname(start, end):
+#    """Names the save folder for a given city pair"""
+#    working_save_dir_name = "_".join([start, "to", end])
+#    config.WORKING_SAVE_DIR_NAME = working_save_dir_name
+#    return working_save_dir_name
 
-
-def create_workingcachedirectory(working_cache_name):
+def create_working_cache_directory(working_cache_name):
     """Creates the cache folder for a given city pair"""
     working_cache_directory = config.CACHE_DIRECTORY + working_cache_name + "/"
     config.WORKING_CACHE_DIRECTORY = working_cache_directory
@@ -66,58 +64,51 @@ def create_workingcachedirectory(working_cache_name):
         os.makedirs(working_cache_directory)
     return working_cache_directory
 
+#def create_graphsdirectory(working_save_directory):
+#    """Creates the folder used to save completed routes"""
+#    graphs_directory = working_save_directory + "graphs/"
+#    config.WORKING_GRAPHS_DIRECTORY = graphs_directory
+#    if not os.path.exists(graphs_directory):
+#        os.makedirs(graphs_directory)
+#    return graphs_directory
 
-def create_graphsdirectory(working_save_directory):
-    """Creates the folder used to save completed routes"""
-    graphs_directory = working_save_directory + "graphs/"
-    config.WORKING_GRAPHS_DIRECTORY = graphs_directory
-    if not os.path.exists(graphs_directory):
-        os.makedirs(graphs_directory)
-    return graphs_directory
+#def create_workingsavedirectory(working_save_dir_name):
+#    """Creates the save directory for a given city pair"""
+#    working_save_directory = config.SAVE_DIRECTORY + working_save_dir_name + "/"
+#    create_graphsdirectory(working_save_directory +
+#                           working_save_dir_name + "_")
+#    config.WORKING_SAVE_DIRECTORY = working_save_directory
+#    if not os.path.exists(working_save_directory):
+#        os.makedirs(working_save_directory)
+#    return working_save_directory
 
-
-def create_workingsavedirectory(working_save_dir_name):
-    """Creates the save directory for a given city pair"""
-    working_save_directory = config.SAVE_DIRECTORY + working_save_dir_name + "/"
-    create_graphsdirectory(working_save_directory +
-                           working_save_dir_name + "_")
-    config.WORKING_SAVE_DIRECTORY = working_save_directory
-    if not os.path.exists(working_save_directory):
-        os.makedirs(working_save_directory)
-    return working_save_directory
-
-
-def create_necessaryfolders(start, end):
+def create_necessary_folders(start, end):
     """Creates all of the cache and save folders"""
     create_basefolders()
     working_cache_name = create_workingcachename(start, end)
-    working_save_dir_name = create_workingsavedirname(start, end)
-    create_workingcachedirectory(working_cache_name)
-    create_workingsavedirectory(working_save_dir_name)
+    #working_save_dir_name = create_workingsavedirname(start, end)
+    create_working_cache_directory(working_cache_name)
+    #create_workingsavedirectory(working_save_dir_name)
 
-
-def get_object_cachepath(object_name):
+def get_object_cache_path(object_name):
     """Gets the path where the object will be cached"""
     object_file_base = "_".join([config.WORKING_CACHE_NAME, object_name])
     object_file_name = object_file_base + ".p"
     object_path = config.WORKING_CACHE_DIRECTORY + object_file_name
     return object_path
 
-
-def get_object_savepath(object_name):
-    """Gets the path where the object will be saved"""
-    object_file_base = "_".join([config.WORKING_SAVE_DIR_NAME, object_name])
-    object_path = config.WORKING_SAVE_DIRECTORY + object_file_base
-    return object_path
-
+#def get_object_savepath(object_name):
+#    """Gets the path where the object will be saved"""
+#    object_file_base = "_".join([config.WORKING_SAVE_DIR_NAME, object_name])
+#    object_path = config.WORKING_SAVE_DIRECTORY + object_file_base
+#    return object_path
 
 def cache_object(in_object, object_name):
     """Caches the object for future use"""
-    object_cache_path = get_object_cachepath(object_name)
+    object_cache_path = get_object_cache_path(object_name)
     file_handle = open(object_cache_path, "wb")
     pickle.dump(in_object, file_handle)
     file_handle.close()
-
 
 def load_object(object_name):
     """Loads the cached object"""
@@ -127,20 +118,17 @@ def load_object(object_name):
     file_handle.close()
     return loaded_object
 
-
 def is_object_cached(object_name):
     """Returns a boolean indicating whether the object is cached"""
     object_cache_path = get_object_cachepath(object_name)
     object_cached = os.path.isfile(object_cache_path)
     return object_cached
 
-
-def is_object_saved(object_name):
-    """Returns a boolean indicated whether the object is saved"""
-    object_save_path = get_object_savepath(object_name)
-    object_saved = os.path.isfile(object_save_path)
-    return object_saved
-
+#def is_object_saved(object_name):
+#    """Returns a boolean indicated whether the object is saved"""
+#    object_save_path = get_object_savepath(object_name)
+#    object_saved = os.path.isfile(object_save_path)
+#    return object_saved
 
 def get_object(object_name, compute_function, compute_args, flag, is_skipped):
     """Either computes the object or loads a cached version"""
@@ -209,9 +197,9 @@ SKIP_PATHS_4D = False
 ###############################
 
 CACHE_DIRECTORY = ""
-SAVE_DIRECTORY = ""
+#SAVE_DIRECTORY = ""
 WORKING_CACHE_NAME = ""
-WORKING_SAVE_DIR_NAME = ""
+#WORKING_SAVE_DIR_NAME = ""
 WORKING_CACHE_DIRECTORY = ""
-WORKING_SAVE_DIRECTORY = ""
+#WORKING_SAVE_DIRECTORY = ""
 WORKING_GRAPHS_DIRECTORY = ""
